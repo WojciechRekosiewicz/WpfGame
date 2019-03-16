6824539414d7d036ea122f04be6919300a8da63b
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-03-16 22:04:49 +0100

    code refactor

--- GameWpf/GameWpf/MainWindow.xaml.cs
+++ GameWpf/GameWpf/MainWindow.xaml.cs
@@ -24,82 +24,76 @@ namespace GameWpf
 
     public partial class MainWindow : Window
     {
-        
-        int speed = 5;
         int ballX = 5;
         int ballY = 5;
         int score = 0;
         bool pause = false;
-       
 
-     
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.PreviewKeyDown += new KeyEventHandler(HandleEsc);
+            this.PreviewKeyDown += new KeyEventHandler(BarMovment);
+            this.PreviewKeyDown += new KeyEventHandler(Pause);
 
-        private void GoodTimer()
+            BallAnimation();
+            Timer();
+
+        }
+
+        private void Timer()
         {
             System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
             dispatcherTimer.Tick += DispatcherTimer_Tick;
             dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
             dispatcherTimer.Start();
-
-            if (pause)
-            {
-                MessageBox.Show("Stop");
-                dispatcherTimer.Stop();
-            }
-
-        
-           
         }
 
-        public static bool CheckCollision(FrameworkElement a, FrameworkElement b)
+        public static bool CheckCollisionBallWithBar(FrameworkElement a, FrameworkElement b)
         {
             Rect rect1 = new Rect((double)a.GetValue(Canvas.LeftProperty), (double)a.GetValue(Canvas.TopProperty), a.Width, a.Height);
             Rect rect2 = new Rect((double)b.GetValue(Canvas.LeftProperty), (double)b.GetValue(Canvas.TopProperty), b.Width, b.Height);
 
             if (rect1.IntersectsWith(rect2))
             {
-                Console.WriteLine("true");
                 return true;
-
             }
-
             else
             {
-                Console.WriteLine("false");
                 return false;
             }
         }
 
         private void DispatcherTimer_Tick(object sender, EventArgs e)
         {
-            PlayerScore.Text = String.Format("{0} :X  {1} :Y ::: {2} : Height {3} : Width, SCORE: {4}", Canvas.GetLeft(Ball1), Canvas.GetTop(Ball1), canvas.ActualWidth, canvas.ActualHeight, score) ;
-            Canvas.SetTop(Ball1, Canvas.GetTop(Ball1) - ballY);
-            Canvas.SetLeft(Ball1, Canvas.GetLeft(Ball1) - ballX);
+            Canvas.SetTop(Ball, Canvas.GetTop(Ball) - ballY);
+            Canvas.SetLeft(Ball, Canvas.GetLeft(Ball) - ballX);
+            PlayerScore.Text = String.Format("{0} :X  {1} :Y ::: {2} : Height {3} : Width, SCORE: {4}", Canvas.GetLeft(Ball), Canvas.GetTop(Ball), canvas.ActualWidth, canvas.ActualHeight, score);
             Pos.Text = String.Format("Bar X :{0} Bar  Y: {1} {2}", Canvas.GetLeft(Bar), Canvas.GetTop(Bar), pause);
 
             PauseHandler();
 
-            if (CheckCollision(Ball1, Bar))
+            if (CheckCollisionBallWithBar(Ball, Bar))
                 {
                     ballY = -ballY;
                     score += 1;
                 }
-                if (Canvas.GetLeft(Ball1) <= 1)
+                if (Canvas.GetLeft(Ball) <= 1)
                 {
                     ballX = -ballX;
                 }
 
-                if (Canvas.GetLeft(Ball1) >= 770)
+                if (Canvas.GetLeft(Ball) >= 770)
                 {
                     ballX = -ballX;
                 }
 
-                if (Canvas.GetTop(Ball1) < 0 || Canvas.GetTop(Ball1) + Ball1.Height > 400)
+                if (Canvas.GetTop(Ball) < 0 || Canvas.GetTop(Ball) + Ball.Height > 400)
                 {
                     ballY = -ballY;
                 }
 
-                if (Canvas.GetTop(Ball1) >= 380)
+                if (Canvas.GetTop(Ball) >= 380)
                 {
                     score -= 1;
                 }
@@ -122,33 +116,6 @@ namespace GameWpf
             }
         }
 
-        private void MoveRight()
-        {
-
-            DoubleAnimation doubleAnimation = new DoubleAnimation();
-            doubleAnimation.To = 760;
-            doubleAnimation.From = 0;
-            doubleAnimation.AutoReverse = true;
-            doubleAnimation.RepeatBehavior = RepeatBehavior.Forever;
-            doubleAnimation.Duration = TimeSpan.FromSeconds(2);
-
-            Ball.BeginAnimation(Canvas.LeftProperty, doubleAnimation);
-
-        }
-
-
-        public MainWindow()
-        {
-            InitializeComponent();
-            this.PreviewKeyDown += new KeyEventHandler(HandleEsc);
-            this.PreviewKeyDown += new KeyEventHandler(HandleBar);
-            this.PreviewKeyDown += new KeyEventHandler(Pause);
-
-            MoveRight();
-            GoodTimer();
-            
-        }
-
         private void Pause(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Space)
@@ -168,34 +135,50 @@ namespace GameWpf
         {
             if (e.Key == Key.Escape)
             {
-                MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Are you sure?", "Delete Confirmation",
+                MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("You want to quit the game?", "Quit",
                     System.Windows.MessageBoxButton.YesNo);
+
                 if (messageBoxResult == MessageBoxResult.Yes)
                 {
                     Close();
                 }
+
             }
         }
 
-        public void HandleBar(object sender, KeyEventArgs e)
+        public void BarMovment(object sender, KeyEventArgs e)
         {
+            int MoveUnit = 10;
+            int LeftSide = 0;
+            int RightSide = 459;
+
             if(e.Key == Key.Left)
             {
-                if(Canvas.GetLeft(Bar) > 0)
+                if(Canvas.GetLeft(Bar) > LeftSide)
                 {
-                    Canvas.SetLeft(Bar, Canvas.GetLeft(Bar) - 10);
+                    Canvas.SetLeft(Bar, Canvas.GetLeft(Bar) - MoveUnit);
                 }
             }
             if (e.Key == Key.Right)
             {
-                if (Canvas.GetLeft(Bar) < 459)
+                if (Canvas.GetLeft(Bar) < RightSide)
                 {
-                    Canvas.SetLeft(Bar, Canvas.GetLeft(Bar) + 10);
+                    Canvas.SetLeft(Bar, Canvas.GetLeft(Bar) + MoveUnit);
                 }
             }
         }
 
         private void BtnCancel_Click(object sender, RoutedEventArgs e) { }
+
+        //To DELETE
+        private void BallAnimation()
+        {
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.To = 760;
+            doubleAnimation.From = 0;
+            doubleAnimation.RepeatBehavior = RepeatBehavior.Forever;
+            Ball2.BeginAnimation(Canvas.LeftProperty, doubleAnimation);
+        }
     }
 }
 
